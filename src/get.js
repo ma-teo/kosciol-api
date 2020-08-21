fcb5bfe269d2be658c3dcdaa464b2e9813990746
mateo <mateo.tarasiuk@gmail.com>
2020-08-21 22:21:13 +0200

    Add options to clearCookie

--- src/get.js
+++ src/get.js
@@ -2,10 +2,15 @@ const data = require('../data/data.json')
 const secret = require('../data/secret.json')
 
 const getData = (req, res) => {
-  secret.token === req.query.token ? res.cookie('token', secret.token, {
+  secret.token === req.query.token
+  ? res.cookie('token', secret.token, {
     domain: process.env.REACT_APP_COOKIE_DOMAIN,
     path: '/'
-  }) : res.clearCookie('token')
+  })
+  : res.clearCookie('token', {
+    domain: process.env.REACT_APP_COOKIE_DOMAIN,
+    path: '/'
+  })
   res.json(data)
 }
 
